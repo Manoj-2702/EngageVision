fd0d74d401fd6f459c3d1b870bae4150f5f39a22
Manoj Kumar H S <hsmanojkumar2003@gmail.com>
2023-12-25 13:25:01 +0530

    Emotion Analysis Added

--- eye_nose_detector.py
+++ eye_nose_detector.py
@@ -15,12 +15,22 @@ from scipy.ndimage import zoom
 from scipy.spatial import distance
 import imutils
 from scipy import ndimage
+
+import dlib
+from tensorflow.keras.models import load_model
+from imutils import face_utils
+import requests
 ############## PARAMETERS #######################################################
 
 # Set these values to show/hide certain vectors of the estimation
 draw_gaze = True
 draw_full_axis = True
 draw_headpose = False
+
+global shape_x
+global shape_y
+global input_shape
+global nClasses
 
 # Gaze Score multiplier (Higher multiplier = Gaze affects headpose estimation more)
 x_score_multiplier = 4
@@ -37,6 +47,16 @@ y = 0                                       # Y axis head pose
 
 X_AXIS_CHEAT = 0
 Y_AXIS_CHEAT = 0
+
+shape_x = 48
+shape_y = 48
+input_shape = (shape_x, shape_y, 1)
+nClasses = 7
+
+thresh = 0.25
+frame_check = 20
+thresh = 0.25
+frame_check = 20
 
 statements=[]
 statements2=[]
@@ -74,11 +94,88 @@ reye_3d[:,2] += 135
 last_lx, last_rx = 0, 0
 last_ly, last_ry = 0, 0
 
+model = load_model('Models/video.h5')
+face_detect = dlib.get_frontal_face_detector()
+predictor_landmarks  = dlib.shape_predictor("Models/face_landmarks.dat")
+
+
+(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+
+(nStart, nEnd) = face_utils.FACIAL_LANDMARKS_IDXS["nose"]
+(mStart, mEnd) = face_utils.FACIAL_LANDMARKS_IDXS["mouth"]
+(jStart, jEnd) = face_utils.FACIAL_LANDMARKS_IDXS["jaw"]
+
+(eblStart, eblEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eyebrow"]
+(ebrStart, ebrEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eyebrow"]
+
+def eye_aspect_ratio(eye):
+    A = distance.euclidean(eye[1], eye[5])
+    B = distance.euclidean(eye[2], eye[4])
+    C = distance.euclidean(eye[0], eye[3])
+    ear = (A + B) / (2.0 * C)
+    return ear
+
+def detect_face(frame):
+    
+    #Cascade classifier pre-trained model
+    cascPath = 'Models/face_landmarks.dat'
+    faceCascade = cv2.CascadeClassifier(cascPath)
+    
+    #BGR -> Gray conversion
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+    #Cascade MultiScale classifier
+    detected_faces = faceCascade.detectMultiScale(gray,scaleFactor=1.1,minNeighbors=6,
+                                                minSize=(shape_x, shape_y),
+                                                flags=cv2.CASCADE_SCALE_IMAGE)
+    coord = []
+                                                
+    for x, y, w, h in detected_faces :
+        if w > 100 :
+            sub_img=frame[y:y+h,x:x+w]
+            cv2.rectangle(frame,(x,y),(x+w,y+h),(0, 255,255),1)
+            coord.append([x,y,w,h])
+
+    return gray, detected_faces, coord
+
+
+def extract_face_features(faces, offset_coefficients=(0.075, 0.05)):
+    gray = faces[0]
+    detected_face = faces[1]
+    
+    new_face = []
+    
+    for det in detected_face :
+        #Region dans laquelle la face est détectée
+        x, y, w, h = det
+        #X et y correspondent à la conversion en gris par gray, et w, h correspondent à la hauteur/largeur
+        
+        #Offset coefficient, np.floor takes the lowest integer (delete border of the image)
+        horizontal_offset = np.int(np.floor(offset_coefficients[0] * w))
+        vertical_offset = np.int(np.floor(offset_coefficients[1] * h))
+        
+        #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        #gray transforme l'image
+        extracted_face = gray[y+vertical_offset:y+h, x+horizontal_offset:x-horizontal_offset+w]
+        
+        #Zoom sur la face extraite
+        new_extracted_face = zoom(extracted_face, (shape_x / extracted_face.shape[0],shape_y / extracted_face.shape[1]))
+        #cast type float
+        new_extracted_face = new_extracted_face.astype(np.float32)
+        #scale
+        new_extracted_face /= float(new_extracted_face.max())
+        #print(new_extracted_face)
+        
+        new_face.append(new_extracted_face)
+    
+    return new_face
+
 while cap.isOpened():
-    success, img = cap.read()
+    success, img1 = cap.read()
 
     # Flip + convert img from BGR to RGB
-    img = cv2.cvtColor(cv2.flip(img, 1), cv2.COLOR_BGR2RGB)
+    img = cv2.cvtColor(cv2.flip(img1, 1), cv2.COLOR_BGR2RGB)
 
     # To improve performance
     img.flags.writeable = False
@@ -92,6 +189,8 @@ while cap.isOpened():
 
     (img_h, img_w, img_c) = img.shape
     face_2d = []
+    gray = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+    rects = face_detect(gray, 1)
 
     if not results.multi_face_landmarks:
       continue 
@@ -319,6 +418,102 @@ while cap.isOpened():
         # cv2.putText(img, f"Gaze Direction: {gaze_direction}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
         cv2.putText(img, text1, (60, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
         cv2.putText(img, text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+    
+    for(i,rect) in enumerate(rects):
+        shape=predictor_landmarks(gray,rect)
+        shape = face_utils.shape_to_np(shape)
+            
+        # Identify face coordinates
+        (x, y, w, h) = face_utils.rect_to_bb(rect)
+        face = gray[y:y+h,x:x+w]
+
+        #Zoom on extracted face
+        face = zoom(face, (shape_x / face.shape[0],shape_y / face.shape[1]))
+            
+        #Cast type float
+        face = face.astype(np.float32)
+            
+        #Scale
+        face /= float(face.max())
+        face = np.reshape(face.flatten(), (1, 48, 48, 1))
+
+        #Make Prediction
+        prediction = model.predict(face)
+        prediction_result = np.argmax(prediction)
+
+        cv2.rectangle(img1, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        
+        cv2.putText(img1, "Face #{}".format(i + 1), (x - 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+     
+        for (j, k) in shape:
+            cv2.circle(img1, (j, k), 1, (0, 0, 255), -1)
+    
+        # 1. Add prediction probabilities
+        cv2.putText(img1, "----------------",(40,100 + 180*i), cv2.FONT_HERSHEY_SIMPLEX, 0.5, 155, 0)
+        cv2.putText(img1, "Emotional report : Face #" + str(i+1),(40,120 + 180*i), cv2.FONT_HERSHEY_SIMPLEX, 0.5, 155, 0)
+        cv2.putText(img1, "Angry : " + str(round(prediction[0][0],3)),(40,140 + 180*i), cv2.FONT_HERSHEY_SIMPLEX, 0.5, 155, 0)
+        cv2.putText(img1, "Disgust : " + str(round(prediction[0][1],3)),(40,160 + 180*i), cv2.FONT_HERSHEY_SIMPLEX, 0.5, 155, 0)
+        cv2.putText(img1, "Fear : " + str(round(prediction[0][2],3)),(40,180 + 180*i), cv2.FONT_HERSHEY_SIMPLEX, 0.5, 155, 1)
+        cv2.putText(img1, "Happy : " + str(round(prediction[0][3],3)),(40,200 + 180*i), cv2.FONT_HERSHEY_SIMPLEX, 0.5, 155, 1)
+        cv2.putText(img1, "Sad : " + str(round(prediction[0][4],3)),(40,220 + 180*i), cv2.FONT_HERSHEY_SIMPLEX, 0.5, 155, 1)
+        cv2.putText(img1, "Surprise : " + str(round(prediction[0][5],3)),(40,240 + 180*i), cv2.FONT_HERSHEY_SIMPLEX, 0.5, 155, 1)
+        cv2.putText(img1, "Neutral : " + str(round(prediction[0][6],3)),(40,260 + 180*i), cv2.FONT_HERSHEY_SIMPLEX, 0.5, 155, 1)
+            
+
+        # 2. Annotate main image with a label
+        if prediction_result == 0 :
+            cv2.putText(img1, "Angry",(x+w-10,y-10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+        elif prediction_result == 1 :
+            cv2.putText(img1, "Disgust",(x+w-10,y-10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+        elif prediction_result == 2 :
+            cv2.putText(img1, "Fear",(x+w-10,y-10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+        elif prediction_result == 3 :
+            cv2.putText(img1, "Happy",(x+w-10,y-10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+        elif prediction_result == 4 :
+            cv2.putText(img1, "Sad",(x+w-10,y-10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+        elif prediction_result == 5 :
+            cv2.putText(img1, "Surprise",(x+w-10,y-10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+        else :
+            cv2.putText(img1, "Neutral",(x+w-10,y-10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+            
+        leftEye = shape[lStart:lEnd]
+        rightEye = shape[rStart:rEnd]
+                
+        # Compute Eye Aspect Ratio
+        leftEAR = eye_aspect_ratio(leftEye)
+        rightEAR = eye_aspect_ratio(rightEye)
+        ear = (leftEAR + rightEAR) / 2.0
+
+        # And plot its contours
+        leftEyeHull = cv2.convexHull(leftEye)
+        rightEyeHull = cv2.convexHull(rightEye)
+        cv2.drawContours(img1, [leftEyeHull], -1, (0, 255, 0), 1)
+        cv2.drawContours(img1, [rightEyeHull], -1, (0, 255, 0), 1)
+            
+        # 4. Detect Nose
+        nose = shape[nStart:nEnd]
+        noseHull = cv2.convexHull(nose)
+        cv2.drawContours(img1, [noseHull], -1, (0, 255, 0), 1)
+
+        # 5. Detect Mouth
+        mouth = shape[mStart:mEnd]
+        mouthHull = cv2.convexHull(mouth)
+        cv2.drawContours(img1, [mouthHull], -1, (0, 255, 0), 1)
+            
+        # 6. Detect Jaw
+        jaw = shape[jStart:jEnd]
+        jawHull = cv2.convexHull(jaw)
+        cv2.drawContours(img1, [jawHull], -1, (0, 255, 0), 1)
+            
+        # 7. Detect Eyebrows
+        ebr = shape[ebrStart:ebrEnd]
+        ebrHull = cv2.convexHull(ebr)
+        cv2.drawContours(img1, [ebrHull], -1, (0, 255, 0), 1)
+        ebl = shape[eblStart:eblEnd]
+        eblHull = cv2.convexHull(ebl)
+        cv2.drawContours(img1, [eblHull], -1, (0, 255, 0), 1)
+
+    cv2.putText(img1,'Number of Faces : ' + str(len(rects)),(40, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, 155, 1)
     cv2.imshow('Engagement Analysis', img)
 
     if cv2.waitKey(5) & 0xFF == ord('q'):
